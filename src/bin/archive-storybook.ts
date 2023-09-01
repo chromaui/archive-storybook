19b5ac219ed24910b5305abd4b532b72b194b9b3
Todd Evanoff <evanoff@gmail.com>
2023-09-01 10:53:06 -0700

    execute file in a more durable manner
    This won't break when there are spaces and other special characters in the file path.

--- src/bin/archive-storybook.ts
+++ src/bin/archive-storybook.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-import { execSync } from 'child_process';
+import { execFileSync } from 'child_process';
 import { resolve, dirname } from 'path';
 
 const configDir = 'node_modules/@chromaui/archive-storybook/config';
 const binPath = resolve(dirname(require.resolve('@storybook/cli/package.json')), './bin/index.js');
-execSync(`node ${binPath} dev -c ${configDir}`, { stdio: 'inherit' });
+execFileSync(binPath, ['dev', '-c', configDir], { stdio: 'inherit' });
